85be75b06b7211c7443254aea4c7c922b3404770
Sam Stratter <humanharddrive@gmail.com>
2018-03-24 23:20:25 -0400

    Queue handling

--- Debug/SerialPassthrough.d
+++ Debug/SerialPassthrough.d
@@ -1,7 +1,13 @@
 SerialPassthrough.d SerialPassthrough.o: .././SerialPassthrough.c \
  .././SerialPassthrough.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint-gcc.h
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint-gcc.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\string.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h
 
 .././SerialPassthrough.h:
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint-gcc.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\string.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h:
--- SerialPassthrough.c
+++ SerialPassthrough.c
@@ -1,9 +1,32 @@
 //SerialPasthrough.c
 #include "SerialPassthrough.h"
 
+#include <string.h>
+
 PASSTHROUGH_PACKET l_BuildPacket;
+
 PASSTHROUGH_PACKET rxBuffer[PASSTHROUGH_RX_BUFFER_SIZE];
 PASSTHROUGH_PACKET txBuffer[PASSTHROUGH_TX_BUFFER_SIZE];
+uint8_t rxIn = 0, rxOut = 0, rxCount = 0;
+uint8_t txIn = 0, txOut = 0, txCount = 0;
+
+static void l_PushQ(PASSTHROUGH_PACKET* pkt, PASSTHROUGH_PACKET* q, uint8_t* qIndex, uint8_t qSize)
+{
+	memcpy(&q[*qIndex], pkt, sizeof(PASSTHROUGH_PACKET));
+	
+	(*qIndex)++;
+	if(*qIndex >= qSize)
+		*qIndex = 0;
+}
+
+static void l_PopQ(PASSTHROUGH_PACKET* pkt, PASSTHROUGH_PACKET* q, uint8_t* qIndex, uint8_t qSize)
+{
+	memcpy(pkt, &q[*qIndex], sizeof(PASSTHROUGH_PACKET));
+	
+	(*qIndex)++;
+	if(*qIndex >= qSize)
+		*qIndex = 0;
+}
 
 void Passthrough_Background(uint8_t c)
 {
